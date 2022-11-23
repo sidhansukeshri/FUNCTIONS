4c385849e36ac8bd052a2663b5e8ff1756274920
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-23 21:05:17 +0530

    Create 11.Call by value.c

---
+++ 11.Call by value.c
@@ -0,0 +1,21 @@
+//My reg no is RA2211042010032
+#include <stdio.h>
+void swap(int x, int y);
+int main ()
+{
+ int a = 560;
+ int b = 690;
+ printf("Before swap, value of a : %d\n", a );
+ printf("Before swap, value of b : %d\n", b );
+ swap(a, b);
+ printf("After swap, value of a : %d\n", a );
+ printf("After swap, value of b : %d\n", b );
+}
+void swap(int x, int y)
+{
+ int temp;
+ temp = x; 
+ x = y; 
+ y = temp; 
+ return;
+}
